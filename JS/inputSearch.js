827749fb774427599374817fc06baee579471ef9
dev-rioma <dev.rioma@gmail.com>
2023-04-11 17:21:53 -0500

    create Products screen and bd.json

--- JS/inputSearch.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector('.search_bar');
-
-toggleButton.addEventListener('click', function() {
-    this.classList.toggle('active');
-    console.log(toggleButton)
-});
